5976161be23d5c3d0e981c9ab1079974c706b019
jin <hyunjin1292@naver.com>
2022-06-07 21:10:08 +0900

    [C] codeup-1092

---
+++ 함께 문제 푸는 날.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main() {
+	int a, b, c, day;
+	day = 1;
+	scanf("%d %d %d", &a, &b, &c);
+	while (day % a != 0 || day % b != 0 || day % c != 0) {
+		day++;
+	}
+	printf("%d", day);
+}
